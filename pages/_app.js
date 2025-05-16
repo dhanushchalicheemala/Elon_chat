1f7f82fbaf4b38416c6ddd7b97e028e535d67b08
Dhanush Chalicheemala <chalicheemala.dhanush@gmail.com>
2025-05-16 01:08:36 -0700

    Add Vercel Analytics to track visitors

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../globals.css';
 import Navigation from '../components/Navigation';
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navigation />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
